8b4a8820ef87f20c3aaf11742f221c29c07fd416
David172082 <david172082@gmail.com>
2025-08-26 23:44:26 -0500

    actualizado

--- src/main/kotlin/com/tuempresa/tasktracker/utils/FechaHelper.kt
+++ src/main/kotlin/com/tuempresa/tasktracker/utils/FechaHelper.kt
@@ -1,20 +1,26 @@
 package com.herreradavid.tasktracker.utils
 
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+
 object FechaHelper {
     const val FORMATO_FECHA = "dd/MM/yyyy"
+    const val FORMATO_HORA = "HH:mm"
 
     fun obtenerFechaActual(): String {
-        // Por ahora hardcodeado, luego se puede usar LocalDate.now()
-        return "20/08/2025"
+        val fecha = LocalDate.now()
+        val formato = DateTimeFormatter.ofPattern(FORMATO_FECHA)
+        return fecha.format(formato)
     }
 
     fun obtenerHoraActual(): String {
-        // Por ahora hardcodeado, luego se puede usar LocalTime.now()
-        return "14:30"
+        val hora = LocalTime.now()
+        val formato = DateTimeFormatter.ofPattern(FORMATO_HORA)
+        return hora.format(formato)
     }
 
     fun formatearFechaHora(): String {
-        // Combina fecha y hora
         return "${obtenerFechaActual()} ${obtenerHoraActual()}"
     }
 }
